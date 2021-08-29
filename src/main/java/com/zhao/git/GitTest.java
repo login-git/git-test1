41b4f9d2551bca7eeb140b77efe2dc44ad43544b
root <zhaoshiji2020@163.com>
2021-08-30 02:11:02 +0800

    first commit IDEA

---
+++ src/main/java/com/zhao/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhao.git;
+
+/**
+ * @Author Zhao
+ * @Date 2021-08-30 上午 2:08
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
